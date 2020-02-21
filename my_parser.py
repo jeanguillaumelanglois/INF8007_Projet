97f75b04f5e6c8685ac75303f92b43fa778fda2a
Marc HOUNTO <mghounto@gmail.com>
2020-02-21 14:18:10 -0500

    pylint apllied to the files my_parser.py and web_crawler.py

--- my_parser.py
+++ my_parser.py
@@ -1,9 +1,16 @@
 # coding=utf-8
+"""importe le module htmlParser
+"""
 from html.parser import HTMLParser
 
 
 class HTMLTagParser(HTMLParser):
+    """La classe utilisee ici pour le HTMLparser est issue de la classe HTMLParser
+            avec les attributs suivants:
+            links = liens resolus via href
+            links_texts = liens resolus via text
 
+           """
     def __init__(self):
         super().__init__()
         self.reset()
@@ -11,6 +18,9 @@ class HTMLTagParser(HTMLParser):
         self.links_text = []
 
     def handle_starttag(self, tag, attrs):
+        """extrait  balises html et les filtres selon qu'elles soient de types <a>
+            si c'est le cas elle les ajoute dans links sinon dans links_texts
+        """
         forbidden_tags = ['data-srcset', 'srcset']
         if tag != 'a':
             attr = dict(attrs)
@@ -19,5 +29,3 @@ class HTMLTagParser(HTMLParser):
             if tag not in forbidden_tags:
                 attr = dict(attrs)
                 self.links.append(attr)
-
-
--- web_crawler.py
+++ web_crawler.py
@@ -1,15 +1,32 @@
+"""requests pour executer des requetes https sur des url,
+    tdlextract est un module de parsage d'url pour extraire un domaine
+    et html parser un module qu'on a cree pour parser le html recu
+   """
 import requests
 from tldextract import extract
 from web_scraper import get_links_on_page
 
 
 class WebCrawler():
+    """La classe utilisee pour le web crawler elle a les attributs suivants :
+        valid_links = liste de liens valides
+        invalid_Links = liste de liens invalides
+        all_links = liste de tous les liens dans le site
+        mis a jour reguliererment au fur et a mesure
+        qu'on parcours des liens
+       """
+
     def __init__(self):
         self.valid_links = []
         self.invalid_links = []
         self.all_links = []
 
     def check_link_validity(self, link_to_check):
+        """params: link_to check (un lien)
+                verifie si le lien retourne un code d'erreur
+                 si oui il le place dans les liens invalides
+                    sinon il le place dans les liens valides.
+               """
         try:
             code = requests.head(link_to_check).status_code
             if code in [400, 403, 404, 405]:
@@ -20,18 +37,25 @@ class WebCrawler():
             self.invalid_links.append((link_to_check, "invalid"))
 
     def extract_domain(self, url):
+        """ params: notre url
+                   permet d'extraire le domaine lie a notre url passee en parametre
+               """
         tsd, td, tsu = extract(url)  # prints abc, hostname, com
         domain = td + '.' + tsu  # will prints as hostname.com
         return domain
 
     def check_domain(self, url, domain):
+        """params = url(notre url), domaine=(notre domaine de reference)
+                   verifie si le domaine de notre url est le meme que le domaine de reference
+              """
         domain_to_check = self.extract_domain(url)
-        if domain == domain_to_check:
-            return True
-        else:
-            return False
+        return domain == domain_to_check
 
     def crawl_site(self, starting_url):
+        """re est un module pour les expressions regest,
+                requests pour executer des requetes https sur des url
+                et html parser un module qu'on a cree pour parser le html recu
+               """
         starting_domain = self.extract_domain(starting_url)
         all_links = get_links_on_page(starting_url, self.all_links)
         for link in all_links:
@@ -42,6 +66,8 @@ class WebCrawler():
             self.print_report()
 
     def print_report(self):
+        """imprime lorsque le crawling est fini les liens valides et les liens invalides
+               """
         print("list of valid links")
         print(self.valid_links)
         print("list of invalid links")
