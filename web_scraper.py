8c7186695322fc5145d40a69324eb22e4725d5aa
Marc HOUNTO <mghounto@gmail.com>
2020-02-21 13:27:12 -0500

    merge and pylint apllied to the file wen_scrapper.py

--- web_scraper.py
+++ web_scraper.py
@@ -1,25 +1,37 @@
+"""re est un module pour les expressions regest,
+requests pour executer des requetes https sur des url
+et html parser un module qu'on a cree pour parser le html recu
+   """
+import re
+import requests
 from my_parser import HTMLTagParser
 
-import requests
-import re
 
 
 def get_links_on_page(url, all_links):
+    """retourne tous les liens contenus dans la page associée au lien passé en parametres
+        On charge le HTML de la page puis on le parse pour recuperer les attributs via les tags
+        Ensuite on fait matcher les valeurs avec notre regex cree pour filtrer les liens parmi
+        d'autres attributs. On vérifie qu'elle n'existe pas deja dans
+        notre liste de liens en parametres avant de l'ajouter
+    """
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 \
+        (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     page = requests.get(url, headers=headers)
     data = page.text
     parser = HTMLTagParser()
     parser.feed(data)
 
     pattern = re.compile("((http|ftp)s?://.*?)")
-    for l in parser.links:
-        for value in list(l.values()):
+    for link in parser.links:
+        for value in list(link.values()):
             if pattern.match(str(value)):
                 if value not in all_links:
                     all_links.append(value)
-    for l in parser.links_text:
-        for value in list(l.values()):
+    print(parser.links_text)
+    for link in parser.links_text:
+        for value in list(link.values()):
             if pattern.match(str(value)):
                 if value not in all_links:
                     all_links.append(value)
