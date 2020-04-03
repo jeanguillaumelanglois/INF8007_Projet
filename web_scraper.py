d4bd6160b47aa3faa1c0ea00929334de2fb2e519
Jean-Guillaume <jeanguillaumel@hotmail.com>
2020-04-03 16:12:16 -0400

    Ajout de la fonction get_links_local_file

--- web_scraper.py
+++ web_scraper.py
@@ -42,3 +42,29 @@ def get_links_on_page_url(url, all_links):
     return all_links
 
 
+def get_links_local_file(local_file_path, all_links):
+    """retourne tous les liens contenus dans la page associée au lien passé en parametres
+        On charge le HTML de la page puis on le parse pour recuperer les attributs via les tags
+        Ensuite on fait matcher les valeurs avec notre regex cree pour filtrer les liens parmi
+        d'autres attributs. On vérifie qu'elle n'existe pas deja dans
+        notre liste de liens en parametres avant de l'ajouter
+    """
+    with open(local_file_path, 'r') as file:
+        data = file.read()
+    parser = HTMLTagParser()
+    parser.feed(data)
+
+    pattern1 = re.compile("((http|ftp)s?://.*?)")
+    pattern2 = re.compile("\/+(.*)")
+    for link in parser.links:
+        for value in list(link.values()):
+            if pattern1.match(str(value)) or pattern2.match(str(value)):
+                if value not in all_links:
+                    all_links.append(value)
+    for link in parser.links_text:
+        for value in list(link.values()):
+            if pattern1.match(str(value)) or pattern2.match(str(value)):
+                if value not in all_links:
+                    all_links.append(value)
+
+    return all_links
